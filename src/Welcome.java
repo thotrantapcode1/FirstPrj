17cf357a221af9ed033782d8ecffd6d705c819e5
thotrantapcode1 <emtradacang040223@gmail.com>
2024-08-14 15:32:36 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Tran Phu Tho!");
+        System.out.println("Welcome to the DHKHDL18A!");
+    }
+}
